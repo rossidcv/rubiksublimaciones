92aed17b183457ffe66a068359988bdf1dc7897b
Monica Rossi <monicarossi3@hotmail.com>
2023-08-02 19:42:42 -0300

    cambios maquetado web

--- js/funciones.js
+++ js/funciones.js
@@ -5,7 +5,7 @@ $(function() {
         $('.logo-white').attr('src', 'imagenes/logo/logo-rubik-alternativo.png')
       }
       if ($(this).scrollTop() < 1) {
-        $('.logo-black').attr('src', 'imagenes/logo/logo-blanco.png');
+        $('.logo-black').attr('src', 'imagenes/logo/logo-rubik-alternativo.png');
       }
     }); 
 });
@@ -21,14 +21,14 @@ $(document).ready(function(){
 
     if(scroll > 0){
       if(!flag){
-        $(".navbar").css({"background-color": "#000"});
+        $(".navbar").css({"background-color": "rgba(0,0,0,0.5)", "backdrop-filter": "blur(10px)"});
         $(".ul.navbar.nav li a").css({"color": "#000", "font-weight": "500"});
         flag = true;
       }
     } 
     else{
       if(flag){
-        $(".navbar").css({"background-color": "rgba(0, 0, 0, 0.5)"});
+        $(".navbar").css({"background-color": "rgba(0,0,0,0.5)", "backdrop-filter": "blur(10px)"});
         $(".ul.navbar.nav li a").css({"color": "#000", "font-weight": "500"});
         flag = false;
       }
